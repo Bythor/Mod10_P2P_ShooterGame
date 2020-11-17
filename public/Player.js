ac4e818179aa78427dfaf6414fa683db28c1c608
Ghidini Sergio <GHIDINIS@tasa.telefonica.com.ar>
2020-11-17 18:30:49 -0300

    First Commit

---
+++ public/Player.js
@@ -0,0 +1,75 @@
+/**
+ * Personaje principal del juego. Hereda de la clase Character.
+ * @extends Character
+ */
+class Player extends Character {
+    /**
+     * Inicializa un jugador
+     * @param game {Game} La instancia del juego al que pertenece el jugador
+     */
+    constructor (game) {
+        const height = PLAYER_HEIGHT * game.width / 100,
+            width = PLAYER_WIDTH * game.width / 100,
+            x = game.width / 2 - width / 2,
+            y = game.height - height,
+            speed = PLAYER_SPEED,
+            myImage = PLAYER_PICTURE,
+            myImageDead = PLAYER_PICTURE_DEAD,
+            score = PLAYER_SCORE,
+            lives = PLAYER_LIVES;
+
+        super(game, width, height, x, y, speed, myImage, myImageDead, score, lives);
+    }
+
+    /**
+     * Actualiza los atributos de posición del jugador y los disparos en función de las teclas pulsadas
+     */
+    update () {
+        if (!this.dead) {
+            switch (this.game.keyPressed) {
+            case KEY_LEFT:
+                if (this.x > this.speed) {
+                    this.x -= this.speed;
+                }
+                break;
+            case KEY_RIGHT:
+                if (this.x < this.game.width - this.width - this.speed) {
+                    this.x += this.speed;
+                }
+                break;
+            case KEY_SHOOT:
+                this.game.shoot(this);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Mata al jugador
+     */
+    collide() {
+        if (!this.dead) {
+            if (this.game.lives < 2) {
+                setTimeout(() => {
+                   this.game.endGame();
+                }, 2000);
+                super.collide();
+            }
+            else {
+                setTimeout(() => {
+                    this.dead = false;
+                    this.image.src = this.myImage;
+                }, 2000);
+            this.game.lives -= 1; // resta 1 vida 
+                super.collide();     
+        }      
+    }
+    }
+}
+  /**
+     * Muestra vidas del jugador
+     */
+const mostrar_lives =() => 
+            {
+        return document.getElementById("livesli").innerHTML = `Lives: ` + this.game.lives;
+                    };
